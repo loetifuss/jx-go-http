105cf490ec4fc2869c7771d1516c11d463d259eb
loetifuss <wolfgang.schmiesing@googlemail.com>
2019-09-24 16:43:25 +0200

    updated with JenkinsX

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example (updated by Wolfgang)"
 
 	from := ""
 	if r.URL != nil {
